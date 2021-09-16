fbea8375b116a72e788c1548981a655c8e494a04
Atmajaya Dega Wiradiana <atmajaya.dw@gmail.com>
2021-09-16 19:22:06 +0700

    Create Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Logo from "../assets/icons/logo.png";
+
+const Navbar = () => {
+  const showNavbar = () => {
+    const span = document.querySelectorAll(".hamburger span");
+    const sidebar = document.querySelector(".sidebar");
+
+    sidebar.classList.toggle("active");
+    span[0].classList.toggle("show1");
+    span[1].classList.toggle("show2");
+    span[2].classList.toggle("show3");
+  };
+
+  return (
+    <>
+      <section id="topbar">
+        <div className="container topbar">
+          <div className="logo">
+            <div className="hamburger" onClick={() => showNavbar()}>
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
+
+            <img src={Logo} className="img-fluid img-logo" alt="" />
+          </div>
+
+          <div className="user">
+            <h5>
+              Hallo, <span>User</span>
+            </h5>
+          </div>
+        </div>
+      </section>
+
+      <section id="sidebar">
+        <div className="sidebar">
+          <div className="logo">
+            <img src={Logo} className="img-fluid img-logo" alt="" />
+          </div>
+
+          <div className="menu">
+            <ul>
+              <li>
+                <a href="!#" className="active">
+                  Dashboard
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Navbar;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-
+import Navbar from "../components/Navbar";
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
+      <>
+        <Navbar />
         <h1>Ini Dashboard</h1>
-      </div>
+      </>
     );
   }
 }
