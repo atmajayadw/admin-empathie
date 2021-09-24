11c652a0bf66fdd873403c023fa5645689645928
Atmajaya Dega Wiradiana <atmajaya.dw@gmail.com>
2021-09-24 18:16:11 +0700

    Create Add Photo

--- src/components/TableClient.js
+++ src/components/TableClient.js
@@ -1,34 +1,110 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import qs from "qs";
 
-const TableClient = () => {
-  return (
-    <>
-      <div className="table-responsive p-3">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Foto</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
+export default class TableClient extends Component {
+  constructor(props) {
+    super(props);
 
-          <tbody>
-            <td>1</td>
-            <td>Foto.jpg</td>
-            <td>
-              <span
-                className="btn btn-danger btn-delete"
-                // onClick={() => this.handleDelete(photos.id)}
-              >
-                Hapus
-              </span>
-            </td>
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
-};
+    this.state = {
+      photos: [],
+      photo: "",
+    };
+  }
 
-export default TableClient;
+  componentDidMount() {
+    this.getAllData();
+  }
+
+  getAllData = () => {
+    const authResult = new URLSearchParams(window.location.search);
+    const client_id = authResult.get("clientID");
+
+    const URL = "http://localhost/empathie-rest-server/api/portfolio/";
+    axios
+      .get(URL, { params: { client_id: client_id } })
+      .then((res) => {
+        const photos = res.data;
+        this.setState({
+          photos: photos.data,
+          photo: photos.data[0].photo,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  handleDelete = (id) => {
+    if (window.confirm("Anda Yakin?")) {
+      const URL = "http://localhost/empathie-rest-server/api/portfolio/";
+      axios({
+        method: "delete",
+        url: URL,
+        data: qs.stringify({
+          id: id,
+        }),
+        headers: {
+          "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      })
+        .then((res) => {
+          alert("berhasil!");
+          this.getAllData();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      return false;
+    }
+  };
+
+  render() {
+    const { photos } = this.state;
+
+    return (
+      <>
+        <div className="table-responsive p-3">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Foto</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {photos &&
+                photos.map((photos, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={
+                          "http://localhost/empathie-rest-server/uploads/" +
+                          photos.photo
+                        }
+                        width={200}
+                        alt=""
+                      />
+                    </td>
+                    <td>
+                      <span
+                        className="btn btn-danger btn-delete"
+                        onClick={() => this.handleDelete(photos.id)}
+                      >
+                        Hapus
+                      </span>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
+}
--- src/pages/AddPhoto.js
+++ src/pages/AddPhoto.js
@@ -1,11 +1,142 @@
 import React, { Component } from "react";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import $ from "jquery";
 
 export default class AddPhoto extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      client_id: "",
+      photo: "",
+      id: "",
+    };
+  }
+
+  componentDidMount() {
+    this.getClientID();
+  }
+
+  getClientID = () => {
+    const authResult = new URLSearchParams(window.location.search);
+    const code = authResult.get("clientID");
+
+    this.setState({
+      client_id: code,
+    });
+  };
+
+  fileSelectedHandler = (event) => {
+    const photo = event.target.files[0];
+
+    // validation
+    if (photo) {
+      if (!photo.name.match(/\.(jpg|jpeg|png|gif)$/)) {
+        alert("Please select valid image.");
+        this.setState({
+          photo: "",
+        });
+        return false;
+      }
+
+      if (photo.size > 2000000) {
+        alert("Images size too large!");
+        this.setState({
+          photo: "",
+        });
+        return false;
+      }
+    }
+
+    this.setState({
+      photo: photo,
+    });
+
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      $("#preview").attr("src", e.target.result);
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  };
+
+  validatingData = () => {
+    const photo = this.state.photo;
+
+    if (photo === "" || photo === undefined) {
+      alert("Please select image.");
+      return false;
+    }
+
+    this.submitData();
+  };
+
+  submitData = () => {
+    const client_id = this.state.client_id;
+
+    // HTTP Request POST
+    const fd = new FormData();
+    fd.append("file", this.state.photo, this.state.photo.name);
+    fd.append("client_id", client_id);
+    fd.append("req", "post");
+    axios
+      .post("http://localhost/empathie-rest-server/api/portfolio/", fd)
+      .then((res) => {
+        console.log(res);
+        alert("file uploaded!");
+        document.location.href = "/dashboard";
+      });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Ini Addphoto</h1>
-      </div>
+      <>
+        <Navbar />
+        <section id="addPhoto">
+          <div className="container addPhoto mt-2">
+            <h2>Add Photo</h2>
+            <form encType="multipart/form-data">
+              <div className="form-group">
+                <label htmlFor="client_id">Client ID</label>
+                <br />
+                <input
+                  id="client_id"
+                  name="client_id"
+                  value={this.state.client_id}
+                  disabled="disabled"
+                  className="w-100"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="photo">Pilih foto:</label>
+                <br />
+                <input
+                  type="file"
+                  id="photo"
+                  name="photo"
+                  onChange={this.fileSelectedHandler}
+                  required
+                />
+              </div>
+
+              <div className="buttons d-flex justify-content-between">
+                <div className="btn btn-primary" onClick={this.validatingData}>
+                  Simpan
+                </div>
+                <Link
+                  to={{
+                    pathname: "/client/",
+                    search: "?clientID=" + this.state.client_id,
+                  }}
+                >
+                  <div className="btn btn-warning">Kembali</div>
+                </Link>
+              </div>
+            </form>
+          </div>
+        </section>
+      </>
     );
   }
 }
